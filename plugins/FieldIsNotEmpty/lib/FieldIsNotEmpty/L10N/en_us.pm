c5de17ccd81194160782aba6feaae3fd77f46a52
okayama <kondoh@alfasado.jp>
2015-01-23 12:42:01 +0900

    initial commit

---
+++ plugins/FieldIsNotEmpty/lib/FieldIsNotEmpty/L10N/en_us.pm
@@ -0,0 +1,7 @@
+package FieldIsNotEmpty::L10N::en_us;
+use strict;
+use base qw/FieldIsNotEmpty::L10N/;
+
+our %Lexicon = ();
+
+1;
---
+++ plugins/FieldIsNotEmpty/lib/FieldIsNotEmpty/L10N/ja.pm
@@ -0,0 +1,11 @@
+package FieldIsNotEmpty::L10N::ja;
+use strict;
+use base 'FieldIsNotEmpty::L10N';
+use vars qw( %Lexicon );
+
+our %Lexicon = (
+    'Modifier field.foo="IS NOT EMPTY" is available in "MTEntries" tag.'
+        => 'MTEntries でモディファイア field.foo="IS NOT EMPTY" を利用できます。',
+);
+
+1;
